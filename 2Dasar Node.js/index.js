cf95d1b3c6a826b687f6647bbf5d3355252fa734
CPuPush <fpakpahan44@gmail.com>
2022-05-21 17:51:18 +0700

    npm

--- 2Dasar Node.js/index.js
+++ 2Dasar Node.js/index.js
@@ -1,4 +1,9 @@
-const message = (name)=>{
-    return `hello ${name}`
-}
-console.log(message("Jennifer"));
+const moment = require('moment');
+// lodash
+const _ = require('lodash');
+
+const date = moment().format('MMM Do YY');
+console.log(date);
+
+const myOddEventArray = _.partition([1,2,3,4,5,6], (n)=>n % 2);
+console.log(myOddEventArray);
